ce18ff06a66473b3e349b9ce0725351e7f753867
aWuttig <andr.wuttig@gmail.com>
2017-10-11 09:20:57 +0200

    * [TASK] Integrates DependsOnModule Interface * [TASK] Fixes truncateTableInDatabase method

--- src/Database.php
+++ src/Database.php
@@ -2,15 +2,43 @@
 
 namespace Codeception\Module\Portrino;
 
+use Codeception\Lib\Interfaces\DependsOnModule;
+use Codeception\Module;
 use Codeception\Module\Db;
-
 
 /**
  * Class Database
  * @package Codeception\Module\Portrino
  */
-class Database extends Db
+class Database extends Module implements DependsOnModule
 {
+    /**
+     * @var string
+     */
+    protected $dependencyMessage = 'Db module is required.';
+
+    /**
+     * @var Db
+     */
+    protected $db;
+
+    /**
+     * @return array
+     * @codeCoverageIgnore
+     */
+    public function _depends()
+    {
+        return [Db::class => $this->dependencyMessage];
+    }
+
+    /**
+     * @param Db $db
+     * @codeCoverageIgnore
+     */
+    public function _inject(Db $db)
+    {
+        $this->db = $db;
+    }
 
     /**
      * Truncate table in database
@@ -21,7 +49,17 @@ class Database extends Db
     public function truncateTableInDatabase($table)
     {
         $query = 'TRUNCATE %s';
+        $query = sprintf($query, $this->db->driver->getQuotedName($table));
         $this->debugSection('Query', $query);
-        $this->driver->executeQuery($query, [$table]);
+        $this->executeQuery($query);
+    }
+
+    /**
+     * @param string $query
+     * @param array $params
+     */
+    protected function executeQuery($query, $params = [])
+    {
+        $this->db->driver->executeQuery($query, $params);
     }
 }
