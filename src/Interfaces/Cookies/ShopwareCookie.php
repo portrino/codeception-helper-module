c585a6b8ffc1900bcf6393e3cdb7567c278cd656
Leopold Engst <engst@portrino.de>
2017-10-16 14:02:34 +0200

    * add Interface Cookie -> ShopwareCookie

---
+++ src/Interfaces/Cookies/ShopwareCookie.php
@@ -0,0 +1,25 @@
+<?php
+namespace Portrino\Codeception\Interfaces\Cookies;
+
+/*
+ * This file is part of the Codeception Helper Module project
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read
+ * LICENSE file that was distributed with this source code.
+ *
+ */
+
+/**
+ * Interface ShopwareCookie
+ *
+ * @package Portrino\Codeception\Interfaces\Cookies
+ */
+interface ShopwareCookie
+{
+   const SHOPWARE_SESSION_COOKIE = 'session-1';
+}
+
