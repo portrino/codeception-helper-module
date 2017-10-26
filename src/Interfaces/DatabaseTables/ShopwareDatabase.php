a977f123d3c2d7da51b43678f774dc46790a27a1
aWuttig <andr.wuttig@gmail.com>
2017-10-26 16:15:46 +0200

    [TASK] Adds some more ShopwareDatabase constants

--- src/Interfaces/DatabaseTables/ShopwareDatabase.php
+++ src/Interfaces/DatabaseTables/ShopwareDatabase.php
@@ -23,6 +23,19 @@ interface ShopwareDatabase
     const S_ARTICLES = 's_articles';
     const S_ARTICLES_DETAILS = 's_articles_details';
 
+    const S_ORDER = 's_order';
+    const S_ORDER_ATTRIBUTES = 's_order_attributes';
+    const S_ORDER_BASKET = 's_order_basket';
+    const S_ORDER_BASKET_ATTRIBUTES = 's_order_basket_attributes';
+    const S_ORDER_BASKET_SIGNATURES = 's_order_basket_signatures';
+    const S_ORDER_BILLINGADDRESS = 's_order_billingaddress';
+    const S_ORDER_BILLINGADDRESS_ATTRIBUTES = 's_order_billingaddress_attributes';
+    const S_ORDER_COMPARISONS = 's_order_basket_signatures';
+    const S_ORDER_DETAILS = 's_order_details';
+    const S_ORDER_DETAILS_ATTRIBUTES = 's_order_details_attributes';
+    const S_ORDER_SHIPPINGADDRESS = 's_order_shippingaddress';
+    const S_ORDER_SHIPPINGADDRESS_ATTRIBUTES = 's_order_shippingaddress_attributes';
+
     const S_USER = 's_user';
     const S_USER_ATTRIBUTES = 's_user_attributes';
     const S_USER_ADDRESSES = 's_user_addresses';
